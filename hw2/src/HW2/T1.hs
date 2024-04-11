b0a54aab13728f0741b7a61dfa39f1044c6baa33
Ljts42 <98562581+Ljts42@users.noreply.github.com>
2024-04-11 11:13:57 +0500

    T1-T2

--- hw2/src/HW2/T1.hs
+++ hw2/src/HW2/T1.hs
@@ -7,4 +7,5 @@ data Tree a = Leaf | Branch !Int (Tree a) a (Tree a)
   deriving (Show)
 
 tfoldr :: (a -> b -> b) -> b -> Tree a -> b
-tfoldr = undefined
+tfoldr _ b Leaf             = b
+tfoldr f b (Branch _ l a r) = tfoldr f (f a $ tfoldr f b r) l
--- hw2/src/HW2/T2.hs
+++ hw2/src/HW2/T2.hs
@@ -3,10 +3,15 @@ module HW2.T2
   , splitOn
   ) where
 
-import Data.List.NonEmpty (NonEmpty)
+import           Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.List (intercalate)
 
 splitOn :: Eq a => a -> [a] -> NonEmpty [a]
-splitOn = undefined
+splitOn sep = foldr add $ pure []
+  where
+    add c (x :| xs) | c == sep  = [] :| (x : xs)
+                    | otherwise = (c : x) :| xs
 
 joinWith :: a -> NonEmpty [a] -> [a]
-joinWith = undefined
+joinWith _ ([] :| []) = []
+joinWith sep (x :| xs) = x ++ (sep : intercalate [sep] xs)
